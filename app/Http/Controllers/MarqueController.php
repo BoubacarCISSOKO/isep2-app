91d4fd136180e67b7f9d01aa30960f7106966c27
Boubacar CISSOKO <cissokomody6@gmail.com>
2022-10-22 18:13:37 +0000

    commit quatre

---
+++ app/Http/Controllers/MarqueController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produit;
+use App\Models\Categorie;
+use App\Models\Marque;
+use Intervention\Image\Facades\Image as Image;
+use Illuminate\Support\Str;
+use Auth;
+
+class MarqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $marques = Marque::orderBy('created_at', 'desc');
+        $marques = $marques->paginate(15);
+        return view('marques.index', compact('marques'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('marques.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+        $marque = new Marque;
+        $marque->nom = $request->nom;
+        $marque->description = $request->description;
+        if ($request->slug != null) {
+            $marque->slug = str_replace(' ', '-', $request->slug);
+        }
+        else {
+            $marque->slug = preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $request->nom)).'-'.Str::random(10);
+        }
+        
+
+        if ($file = $request->file('logo')) {
+
+            $optimizeImage = Image::make($file);
+            $optimizePath = public_path() . '/images/marques/';
+            $image = time() . $file->getClientOriginalExtension();
+            $optimizeImage->resize(200, 200, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $optimizeImage->save($optimizePath . $image, 90);
+
+            $marque->logo = $image;
+        }
+
+        $marque->save(); 
+        return redirect()->route('marques.index')->with('info', 'La catégorie a bien été créée');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,60 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+// produits
+Route::get('/produit', [App\Http\Controllers\ProductController::class, 'index'])->name('products.index');
+Route::get('/produit/cree', [App\Http\Controllers\ProductController::class, 'create'])->name('products.create');
+Route::post('/produit/save', [App\Http\Controllers\ProductController::class, 'store'])->name('products.store');
+
+
+
+// articles
+Route::get('/articles', [App\Http\Controllers\ArticlesController::class, 'index'])->name('articles.index');
+Route::get('/articles/cree', [App\Http\Controllers\ArticlesController::class, 'create'])->name('articles.create');
+Route::post('/articles/save', [App\Http\Controllers\ArticlesController::class, 'store'])->name('articles.store');
+
+
+
+// categories
+Route::get('/categories', [App\Http\Controllers\CategorieController::class, 'index'])->name('categories.index');
+Route::get('/categories/cree', [App\Http\Controllers\CategorieController::class, 'create'])->name('categories.create');
+Route::post('/categories/store', [App\Http\Controllers\CategorieController::class, 'store'])->name('categories.store');
+
+
+
+// souscategories
+Route::get('/soucategories', [App\Http\Controllers\SouscategorieController::class, 'index'])->name('souscategories.index');
+Route::get('/souscategories/cree', [App\Http\Controllers\SouscategorieController::class, 'create'])->name('souscategories.create');
+Route::post('/souscategories/store', [App\Http\Controllers\SouscategorieController::class, 'store'])->name('souscategories.store');
+
+
+
+// marques
+Route::get('/marques', [App\Http\Controllers\MarqueController::class, 'index'])->name('marques.index');
+Route::get('/marques/cree', [App\Http\Controllers\MarqueController::class, 'create'])->name('marques.create');
+Route::post('/marques/store', [App\Http\Controllers\MarqueController::class, 'store'])->name('marques.store');
+
+// panier
+Route::get('cart', [App\Http\Controllers\CartController::class, 'cartList'])->name('cart.list');
+Route::post('cart', [App\Http\Controllers\CartController::class, 'addToCart'])->name('cart.store');
+Route::post('update-cart', [App\Http\Controllers\CartController::class, 'updateCart'])->name('cart.update');
+Route::post('remove', [App\Http\Controllers\CartController::class, 'removeCart'])->name('cart.remove');
+Route::post('clear', [App\Http\Controllers\CartController::class, 'clearAllCart'])->name('cart.clear');
+
+
+//commande
+Route::middleware('auth')->group(function () {
+    Route::get('/commande', [OrdersController::class,'index'])->name('commande.index');
+    Route::post('commande/ajouter', [OrdersController::class,'store'])->name('commande.store');
+    Route::get('commande/confirmation', [OrdersController::class,'confirmation'])->name('commande.confirmation');
+});
+
+//client
+Route::middleware('auth')->group(function () {
+    Route::name('client')->get('compte', [ClientController::class, 'index' ]);
+    Route::name('client.pdf')->get('client/pdf', [ClientController::class, 'pdf' ]);
+    Route::name('client.update')->put('compte', [ClientController::class, 'update' ]);
+});
